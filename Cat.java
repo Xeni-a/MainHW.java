c21484f2abb784adc3e612c93f2db0e40755aa54
Xeni-a <81966190+Xeni-a@users.noreply.github.com>
2021-04-05 15:09:04 +0300

    Add files via upload

---
+++ Cat.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Cat {
+    private String name;
+    public int age;
+
+    public Cat(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
